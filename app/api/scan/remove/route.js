912e8bed802a9edf36c21212c01fb7bd8daeb47e
Snika Kumar <snikakumar04@gmail.com>
2026-04-20 01:05:04 +0530

    Update route.js

--- app/api/scan/remove/route.js
+++ app/api/scan/remove/route.js
@@ -6,20 +6,43 @@ export async function POST(req) {
 
     let cart = global.carts[trolley_id] || [];
 
-    cart = cart.filter(item => item.barcode !== barcode);
+    const index = cart.findIndex(
+      item => item.barcode === barcode
+    );
+
+    if (index === -1) {
+      return Response.json({
+        success: false,
+        message: "Item not found"
+      }, { status: 404 });
+    }
+
+    const removedItem = cart[index];
+
+    // quantity >1 then minus 1
+    if (removedItem.quantity && removedItem.quantity > 1) {
+      cart[index].quantity -= 1;
+    } else {
+      cart.splice(index, 1);
+    }
 
     global.carts[trolley_id] = cart;
 
     return Response.json({
       success: true,
       message: "Item removed",
-      cart: cart
+      product: {
+        name: removedItem.name,
+        price: removedItem.price,
+        barcode: removedItem.barcode
+      },
+      cart
     });
 
   } catch (error) {
     return Response.json({
       success: false,
       error: error.message
-    });
+    }, { status: 500 });
   }
 }
